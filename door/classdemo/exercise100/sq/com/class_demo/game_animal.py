de21d18f45faf61fbcd2f9051cd207b05a3b38ae
coronahana <1224140873@qq.com>
2019-12-03 14:07:11 +0800

    动物喂食小游戏

--- door/classdemo/exercise100/sq/com/class_demo/game_animal.py
+++ door/classdemo/exercise100/sq/com/class_demo/game_animal.py
@@ -1,33 +1,44 @@
 from random  import  randint
 import  time
-
 class Tiger:
     callVoice = "WOW!!"
     nickName="老虎"
-    isHungry = False
-    def __init__(self, inifood, iniweight=200):
+    eatNum = 0 #吃的次数
+    callNum =0 #叫的次数
+    def __init__(self, inifood, inieatMaxNum,inicallMaxNum,iniweight=200):
         self.food = inifood
         self.weight =iniweight
+        self.eatMaxNum = inieatMaxNum
+        self.callMaxNum = inicallMaxNum
     def call(self):
         print(self.callVoice)
-        self.weight-=5 # 如果叫体重就会减少5斤
+        self.weight-=5 # 如果叫了一声体重就会减少5斤
         pass
     def feed(self,food):
-        if(food=="mead"):
-            print("虎喂对了")
-            self.weight += 10
-            self.isHungry = True
+        if (self.eatNum <3):
+            if(food=="meate"):
+                # print("虎喂对了")
+                self.weight += 10
+                self.eatNum += 1
+                return "success"
+
+            else :
+                self.weight -= 10
+                return "fail"
         else :
-            self.weight -= 10
+            print("喂过了,不能再喂了")
+            return "fail"
 
 class Sheep:
     callVoice = "mie!!"
     nickName="羊"
-    isHungry= False#喂过一次就不饿了
-    def __init__(self, inifood, iniweight=100):
+    eatNum = 0  # 吃的次数
+    callNum = 0  # 叫的次数
+    def __init__(self, inifood,inieatMaxNum,inicallMaxNum,iniweight=100):
         self.weight = iniweight
         self.food = inifood
-
+        self.eatMaxNum =inieatMaxNum
+        self.callMaxNum = inicallMaxNum
     def call(self):
         # 如果叫体重就会减少5斤
         print(self.callVoice)
@@ -35,70 +46,94 @@ class Sheep:
         pass
 
     def feed(self,food):
-        if(food == "grass"):
-            print("羊喂对了")
-            self.weight += 10
-            self.isHungry=True
+        if(self.eatNum <3):
+            if(food == "grass"):
+                # print("羊喂对了")
+                self.weight += 10
+                self.eatNum+=1
+                return "success"
+            else :
+                self.weight -= 10
+                return "fail"
         else :
-            self.weight -= 10
+            print("喂过了,不能再喂了")
+            return "fail"
+
 class Room:
     def __init__(self,ininum,inianima):
         self.num = ininum
         self.anima = inianima
 
-def game_demo():
-    print("开始游戏")
+
+def game_demo(game_Maxtime):
     # 记忆房间动物
-    rem={"0":"房间号：动物名"}
-    romList=[]
-    #随机初始化10个房间的动物编号和动物
+    rem={"可喂食房间和动物":""}
+    roomList=[]
+    #随机初始化10个房间的动物编号和动物,每只老虎能喂3次、每只羊只能喂2次，只能叫1次
     for one in range(1,11):
         if randint(0,1):
-            print("初始化一个老虎")
-            ani = Room(str(one),Tiger("meat"))
+            # print("初始化一个老虎")
+            ani = Room(str(one),Tiger("meat",3,1))
         else:
-            print("初始化一只羊")
-            ani=  Room(str(one),Sheep("grass"))
-        romList.append(ani)
-
+            # print("初始化一只羊")
+            ani=  Room(str(one),Sheep("grass",2,1))
+        roomList.append(ani)
+    # 完成初始化
+    print("============================================开始游戏============================================")
+    print ("\n-游戏规则默认每只老虎能喂3次、每只羊只能喂2次，都只能叫1次\n-游戏时间" + str (game_Maxtime) + "s\n")
     startime = time.time()
     while True :
-        # 随机产生一个房间号，让用户选择喂食或者敲门
+        #随机产生一个房间号，让用户选择喂食或者敲门
         room_num =randint(0,9)
-        print("请您去房间号"+str(room_num+1)+"门口选择操作")
-        # choose ="1"
-        choose =""
+        print("请您去房间号"+str(room_num+1)+"门口选择操作 \n")
         while True :
-            choose = input("输入1喂食，输入2敲门！请选择操作，提示"+str(rem))
+            endtime = time.time ()
+            if ((endtime - startime) > int (game_Maxtime)):
+                print ("============================================游戏结束============================================")
+                break
+            choose = input("输入1喂食,输入2敲门,提示"+str(rem)+"\n")
             if(choose=="1" or choose=="2"):
                 break
-
-
+        an = (roomList[room_num]).anima
         if(choose=="1"):
-            # feedfood="meat" #或者grass 用户输入
-            feedfood = input("输入meate或者grass！请输入")
-            ((romList[room_num]).anima.feed(feedfood))
-        else:
-            #选则了敲门2
-            an=(romList[room_num]).anima
-            an.call()
-            if(an.nickName=="老虎"):
-                rem[str((romList[room_num]).num)]="老虎"
+            # 喂食1
+            feedfood = input("输入meate或者grass！请输入 ")
+            res=(an.feed(feedfood))
+            if(res=="success"):
+                print("编号"+str((romList[room_num]).num)+"的"+an.nickName+"成功吃到"+an.food)
+                if rem.keys().__contains__(str((roomList[room_num]).num)):
+                    rem.pop(str((roomList[room_num]).num))#喂过的去掉强提示
+                pass
             else:
-                rem[str((romList[room_num]).num)]= "羊"
+                pass
+        else:
+            #敲门2
 
-        endtime=time.time()
-        if((endtime-startime)>40):
-            print("游戏结束")
+            if an.callNum<an.callMaxNum:
+                an.call()
+                if (an.nickName == "老虎"):
+                    if an.callNum >=an.eatMaxNum:
+                        pass
+                    else:
+                        #添加老虎还能喂强提示2
+                        rem[str((roomList[room_num]).num)] = "老虎"
+                else:
+
+                    if an.callNum >=an.eatMaxNum:
+                        pass
+                    else:
+                        # 添加羊还能喂的强提示
+                        rem[str ((roomList[room_num]).num)] = "羊"
+            else:
+                print (an.nickName + "叫够次数了,不叫了")
+        endtime = time.time ()
+        if ((endtime - startime) > int (game_Maxtime)):
+            print ("============================================游戏结束============================================")
             break
-    for roomone in romList:
-        print(str(roomone.num),roomone.anima.nickName,roomone.anima.isHungry,roomone.anima.weight)
 
-    print(romList)
+    return roomList
 
-game_demo()
-
-
-
-
-
+roomList= game_demo(40)
+for roomone in roomList:
+    print (str (roomone.num), roomone.anima.nickName, roomone.anima.weight)
+#print(romList)
