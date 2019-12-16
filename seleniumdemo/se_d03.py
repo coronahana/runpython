d6348b06c171bf489c338cdc42b560eed460cd06
coronahana <1224140873@qq.com>
2019-12-16 20:25:23 +0800

    提交 se_d03

---
+++ seleniumdemo/se_d03.py
@@ -0,0 +1,235 @@
+"""
+Selenium 作业:
+测试结果
+Python开发工程师|公司名|工作地点|薪资|发布时间
+Python开发工程师|杭州皮森智能科技有限公司|杭州-西湖区|1.5-2万/月|12-15
+Python开发工程师|华为技术有限公司|杭州|1-2万/月|12-15
+Python开发工程师|杭州智一信息技术有限公司|杭州-西湖区|0.8-1万/月|12-15
+Python开发工程师|美国道富|杭州|4-5万/月|12-15
+Python开发工程师|杭州勤鼎科技有限公司|杭州-滨江区|6-8.5千/月|12-15
+Python开发工程师|湖南金烽信息科技有限公司|杭州|1.2-2.4万/月|12-15
+Python开发工程师|杭州中亚瑞程包装科技有限公司|杭州|1-1.5万/月|12-14
+Python开发工程师|杭州沃趣科技股份有限公司|杭州-滨江区|1.6-2万/月|12-14
+Python开发工程师|杭州味捷品牌管理集团有限公司|杭州-滨江区|1-1.5万/月|12-14
+Python开发工程师|杭州瑞东机械有限公司|杭州|1-1.5万/月|12-14
+Python开发工程师|浙江迈睿机器人有限公司|杭州|0.6-1.5万/月|12-14
+Python开发工程师|浙江正洁环境科技有限公司|杭州-江干区|1.5-2万/月|12-14
+Python开发工程师|杭州俊程企业管理咨询有限公司|杭州-江干区|2-2.5万/月|12-14
+Python开发工程师|上海雍米计算机科技有限公司|杭州-滨江区|2-3万/月|12-13
+Python开发工程师|麦田春雪科贸（北京）有限公司|杭州-余杭区|1.5-2万/月|12-13
+Python开发工程师|浙江诺诺网络科技有限公司|杭州-西湖区|1-2万/月|12-13
+Python开发工程师|深圳市若昕科技有限公司|杭州-滨江区|2-3万/月|12-13
+Python开发工程师|杭州金时资产管理有限公司|杭州-西湖区|15-50万/年|12-13
+Python开发工程师|杭州安脉盛智能技术有限公司|杭州|1.5-2.2万/月|12-13
+Python开发工程师|杭州盈丰软件股份有限公司|杭州|1-2万/月|12-13
+Python开发工程师|杭州优灿科技有限公司|异地招聘|1-2万/月|12-13
+Python开发工程师|杭州晟视科技有限公司|异地招聘|1-1.5万/月|12-13
+Python开发工程师|广州华钧软件科技有限公司|杭州-滨江区|2-3万/月|12-13
+Python开发工程师|火石创造（医健产业大数据）|杭州-滨江区|1.5-2.5万/月|12-13
+Python开发工程师|恩士迅信息科技（上海）有限公司|杭州-西湖区|1-1.5万/月|12-13
+Python开发工程师|杭州爱掌科技有限公司|杭州|1-1.5万/月|12-13
+Python开发工程师|北京联龙博通电子商务技术有限公司...|杭州-滨江区|1.2-1.8万/月|12-13
+Python开发工程师|杭州幻码教育科技有限公司|杭州-江干区|0.8-1.5万/月|12-13
+Python开发工程师|上海中电电子系统科技股份有限公司...|杭州-拱墅区|1-2万/月|12-13
+Python开发工程师|浙江元一家居用品集团有限公司|杭州-萧山区|1-1.5万/月|12-13
+Python开发工程师|杭州复杂美科技有限公司|杭州-滨江区|20-50万/年|12-13
+Python开发工程师|深圳市捷兴电子商务有限公司|杭州-滨江区|2-3万/月|12-13
+Python开发工程师|江西高创保安服务技术有限公司杭州...|杭州-江干区|1-1.5万/月|12-13
+Python开发工程师|南京海翊数据技术有限公司|杭州|1.2-2万/月|12-13
+Python开发工程师|杭州祐全科技发展有限公司|杭州-余杭区|1-2.5万/月|12-13
+Python开发工程师|杭州涂鸦信息技术有限公司|杭州-西湖区|1.5-3万/月|12-13
+Python开发工程师|特维轮智车慧达科技（杭州）有限公...|杭州-拱墅区|1.5-2.5万/月|12-13
+Python开发工程师|浙江网新恒天软件有限公司|杭州-西湖区|1.3-1.7万/月|12-12
+Python开发工程师|上海复深蓝软件股份有限公司|杭州-江干区|1.2-1.6万/月|12-12
+Python开发工程师|杭州小码教育科技有限公司|杭州-江干区|0.8-1.5万/月|12-12
+Python开发工程师|浙江银江研究院有限公司|杭州-西湖区|0.8-1.2万/月|12-12
+Python开发工程师|杭州海康威视数字技术股份有限公司...|杭州-滨江区|1-1.5万/月|12-12
+Python开发工程师|浙江力太工业互联网有限公司|杭州-滨江区|1-1.5万/月|12-11
+Python开发工程师|杭州拼图教育科技有限公司|杭州-下城区|0.5-1万/月|12-11
+Python开发工程师|杭州长坂坡网络科技有限公司|杭州-滨江区|1-2.5万/月|12-11
+Python开发工程师|杭州览众数据科技有限公司|杭州-滨江区|1-1.5万/月|12-09
+Python开发工程师|杭州博彦信息技术有限公司|异地招聘|1.2-1.6万/月|12-09
+Python开发工程师|杭州小米智课教育科技有限公司|杭州-下城区|0.7-1.2万/月|12-06
+Python开发工程师|嘉兴构美信息技术有限公司|杭州-江干区|1.2-2万/月|12-05
+Python开发工程师|杭州锐翌基因技术有限公司|杭州|0.9-1.1万/月|12-03
+
+"""
+from selenium import webdriver
+import  time
+def selenium_demo03():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
+    driver.get("http://www.51job.com")
+    temp = driver.find_element_by_id("kwdselectid").send_keys("python")
+    driver.find_element_by_id("work_position_input").click()
+    time.sleep(4)
+    oldcitys = driver.find_elements_by_id("work_position_click_multiple_selected")
+    #删除之前选择的所有城市
+    for oldcity in oldcitys:
+        # print("去掉一个选择的城市")
+        #
+        # work_position_click_multiple_selected_each_060000 > em
+        oldcity.find_element_by_css_selector("span.ttag>  em").click()
+    #选择杭州
+    driver.find_element_by_id("work_position_click_center_right_list_category_000000_080200").click()
+
+    #保存选择
+    driver.find_element_by_id("work_position_click_bottom_save").click()
+    #提交搜索> div.fltr.radius_5 > div > button
+    driver.find_element_by_css_selector("div.fltr.radius_5 > div > button").click()
+    # time.sleep(3)
+    oneItems = driver.find_elements_by_css_selector("#resultList > div.el")
+
+    for one in oneItems:
+        # 职位名 公司名 工作地点 薪资 发布发布
+        jobName=one.find_element_by_xpath("//p/span/a").text
+        compName=one.find_element_by_css_selector("span.t2").text
+        jobCity=one.find_element_by_css_selector("span.t3").text
+        jobSar=one.find_element_by_css_selector("span.t4").text
+        jobSendTime=one.find_element_by_css_selector("span.t5").text
+        print(f'{jobName}|{compName}|{jobCity}|{jobSar}|{jobSendTime}')
+
+    driver.quit()
+if __name__ == '__main__':
+    selenium_demo03()
+    pass
+"""
+Selenium 作业 3
+
+
+
+登录 51job ，
+http://www.51job.com
+
+输入搜索关键词 "python"， 地区选择 "杭州"（注意，如果所在地已经选中其他地区，要去掉）， 
+搜索最新发布的职位， 抓取页面信息。 得到如下的格式化信息
+
+Python开发工程师 | 杭州纳帕科技有限公司 | 杭州 | 0.8-1.6万/月 | 04-27
+Python高级开发工程师 | 中浙信科技咨询有限公司 | 杭州 | 1-1.5万/月 | 04-27
+高级Python开发工程师 | 杭州新思维计算机有限公司 | 杭州-西湖区 | 1-1.5万/月 | 04-27
+
+【参考答案，往下翻】
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+方法一
+# coding:utf8
+from selenium import webdriver
+
+driver = webdriver.Chrome(r"d:\tools\webdrivers\chromedriver.exe")
+# 别忘了设置
+driver.implicitly_wait(10)
+
+# 抓取信息
+driver.get('http://www.51job.com')
+
+driver.find_element_by_id('kwdselectid').send_keys('python')
+# 点击工作地点
+driver.find_element_by_id('work_position_input').click()
+
+
+# 选择所有城市，去掉非杭州的且选择杭州，
+# 如果是杭州但是没有选，选上这些城市
+cityEles = driver.find_elements_by_css_selector('#work_position_click_center_right em')
+
+for one in cityEles:
+    cityName = one.text
+    selected = one.get_attribute('class')
+    # print cityName,seleted
+
+    if cityName == u'杭州':
+        if selected != 'on':
+            one.click()
+
+    else:
+        if selected == 'on':
+            one.click()
+
+# 保存城市选择
+driver.find_element_by_id('work_position_click_bottom_save').click()
+
+# 点击搜索
+driver.find_element_by_css_selector('.ush  button').click()
+
+# 搜索结果分析
+jobs = driver.find_elements_by_css_selector('#resultList  div.el')
+
+for job in jobs:
+    # 去掉第一行：标题行
+    if 'title' in job.get_attribute('class'):
+        continue
+
+    filelds = job.find_elements_by_tag_name('span')
+    strField = [fileld.text for fileld in filelds]
+    print (' | '.join(strField))
+
+
+driver.quit()
+方法二
+# coding:utf8
+from selenium import webdriver
+
+driver = webdriver.Chrome(r"d:\tools\webdrivers\chromedriver.exe")
+driver.implicitly_wait(10)
+
+
+driver.get('http://www.51job.com')
+
+driver.find_element_by_id('kwdselectid').send_keys('python')
+driver.find_element_by_id('work_position_input').click()
+
+
+# 选择城市，去掉非杭州的，选择杭州
+selectedCityEles = driver.find_elements_by_css_selector(
+    '#work_position_click_center_right_list_000000 em[class=on]')
+    
+
+for one in selectedCityEles:
+    one.click()
+
+driver.find_element_by_id('work_position_click_center_right_list_category_000000_080200').click()
+
+# 保存城市选择
+driver.find_element_by_id('work_position_click_bottom_save').click()
+driver.find_element_by_css_selector('div.ush > button').click()
+
+# 搜索结果分析
+jobs = driver.find_elements_by_css_selector('#resultList div[class=el]')
+for job in jobs:
+
+    fields = job.find_elements_by_tag_name('span')
+    stringFilelds = [field.text for field in fields]
+    print (' | '.join(stringFilelds))
+
+
+
+driver.quit()
+"""
